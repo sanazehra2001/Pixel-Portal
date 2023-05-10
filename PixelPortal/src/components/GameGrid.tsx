661441afc969b6331d383533a390c3eea51fbc81
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2023-05-10 14:06:28 +0500

    Create a Generic data fetching Hook

--- PixelPortal/src/components/GameGrid.tsx
+++ PixelPortal/src/components/GameGrid.tsx
@@ -5,19 +5,19 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { data, error } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={10}>
-        {!games?.length &&
+        {!data?.length &&
           skeletons.map((skeleton) => (
             <GameCardContainer children={<GameCardSkeleton key={skeleton} />} />
           ))}
 
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer
             children={<GameCard key={game.id} game={game} />}
           />
