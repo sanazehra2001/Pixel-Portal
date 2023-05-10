6e920ce0822f88507c55c57619370e71c2d9e452
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2023-05-10 17:36:40 +0500

    Build Search Input

--- PixelPortal/src/components/NavBar.tsx
+++ PixelPortal/src/components/NavBar.tsx
@@ -1,11 +1,14 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/controller.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="50px" />
+      <Text whiteSpace={"nowrap"}>Pixel Portal</Text>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
