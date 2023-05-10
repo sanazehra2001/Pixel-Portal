cc91493e4ad0f64867fbd9f6243b74aca19eb054
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2023-05-10 14:25:19 +0500

    Show Spinner while fetching Genres

--- PixelPortal/src/components/GenreList.tsx
+++ PixelPortal/src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error } = useGenres();
+
+  if (!data?.length) return <Spinner />;
+  if (error) return null;
+
   return (
     <List>
       {data.map((genre) => (
